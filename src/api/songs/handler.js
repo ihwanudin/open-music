5e5019af3d21187301a778b0fa2bc4ea5d5b5642
ihwanudin <ihwanoedin@gmail.com>
2021-06-28 07:03:12 +0700

    Submission Submit commented

--- src/api/songs/handler.js
+++ src/api/songs/handler.js
@@ -50,6 +50,7 @@ class SongsHandler {
   }
  
   async getSongsHandler() {
+    try {
     const songs = await this._service.getSongs();
     return {
       status: 'success',
@@ -57,6 +58,25 @@ class SongsHandler {
         songs,
       },
     };
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // Server ERROR!
+      const response = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
   }
  
   async getSongByIdHandler(request, h) {
